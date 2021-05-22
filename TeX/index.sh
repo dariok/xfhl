1aaf4ae34751c34379a8659d14815de0b8785a74
Dario Kampkaspar <dario.kampkaspar@ulb.tu-darmstadt.de>
2021-05-22 22:23:07 +0200

    texindy does not work from latex workshop

--- TeX/index.sh
+++ TeX/index.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
-texindy -C utf8 -M style1.xdy xfhl-top.idx
+#cd $1
+#texindy -C utf8 -M style1.xdy xfhl-top.idx
+#texindy -C utf8 -M style1.xdy xfhl-per.idx
+
+makeindex xfhl-top.idx
+makeindex xfhl-per.idx
+
+exit 0
